af251f97fb3b5dfec2596a21f091164d74471dd0
Osmanli Farid <osmfarid@gmail.com>
2024-06-19 19:26:08 +0400

    [General] Added swup type defs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="astro/client" />
 /// <reference path="../.astro/types.d.ts" />
+
+import type { Swup } from '@swup/astro/client'
+
+declare global {
+  interface Window {
+    swup: Swup
+  }
+}
